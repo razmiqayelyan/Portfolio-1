fa5b05e8024d59e517755035f0639198331f8557
Raz Mikayelyan <razmikayelyan@Razs-MacBook-Air.local>
2022-08-28 14:20:33 +0400

    Portfolio 1

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -90,7 +90,7 @@ function NavBar() {
             </Nav.Item>
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/razmiqayelyan/Portfolio-1"
                 target="_blank"
                 className="fork-btn-inner"
               >
